df3a89827fa968e711d822e88b8c7ba806ca84aa
miruna <58366207+mirunaish@users.noreply.github.com>
2024-12-02 23:44:08 +0200

    added themes

--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -1,9 +1,22 @@
-import bingusIcon from "../assets/Bingus.webp";
 import floppaIcon from "../assets/Pet_floppa.webp";
 import floppaBackground from "../assets/floppa-bg.jpg";
 import floppaDarkBackground from "../assets/floppa-dark.png";
+
+import soggaIcon from "../assets/Sogga.png";
+import soggaBackground from "../assets/sogga-bg.png";
+import soggaDarkBackground from "../assets/sogga-bg-dark.webp";
+
+import bingusIcon from "../assets/Bingus.webp";
 import bingusBackground from "../assets/bingus-bg.jpg";
 import bingusDarkBackground from "../assets/bingus-bg-dark.jpg";
+
+import maxwellIcon from "../assets/maxwell.png";
+import maxwellBackground from "../assets/maxwell-bg.jpg";
+import maxwellDarkBackground from "../assets/maxwell-bg-dark-2.jpg";
+
+import oiiaIcon from "../assets/oiia.png";
+import oiiaBackground from "../assets/oiia-bg.jpg";
+import oiiaDarkBackground from "../assets/oiia-bg-dark.jpg";
 
 export enum ThemeTypes {
   DARK = "dark",
@@ -11,14 +24,16 @@ export enum ThemeTypes {
 }
 
 export enum Themes {
-  FLOPPA = "floppa",
-  BINGUS = "bingus",
+  FLOPPA = "floppa", // reddish brown
+  SOGGA = "sogga", // yellow
+  MAXWELL = "maxwell", // greenish
+  OIIA = "oiia", // blueish
+  BINGUS = "bingus", // purplish pink
 }
 
 export interface Theme {
   primary: string;
   primaryAlt: string;
-  secondary: string;
   background: string;
   backgroundAlt: string;
   text: string;
@@ -31,7 +46,6 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
     [ThemeTypes.DARK]: {
       primary: "#753A22",
       primaryAlt: "#572612",
-      secondary: "#228B22",
       background: "#260C01",
       backgroundAlt: "#4F1802",
       text: "#F7E3D4",
@@ -41,7 +55,6 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
     [ThemeTypes.LIGHT]: {
       primary: "#A86D58",
       primaryAlt: "#8F513B",
-      secondary: "#228B22",
       background: "#FAE9DE",
       backgroundAlt: "#FFF9F5",
       text: "#4D2618",
@@ -49,11 +62,30 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
       backgroundImage: `url(${floppaBackground})`,
     },
   },
+  [Themes.SOGGA]: {
+    [ThemeTypes.DARK]: {
+      primary: "#b59500",
+      primaryAlt: "#946d00",
+      background: "#322400",
+      backgroundAlt: "#573e04",
+      text: "#FFFFE0",
+      textAlt: "#c9c986",
+      backgroundImage: `url(${soggaDarkBackground})`,
+    },
+    [ThemeTypes.LIGHT]: {
+      primary: "#f6e85c",
+      primaryAlt: "#ddca08",
+      background: "#FFFACD",
+      backgroundAlt: "#FFFFE0",
+      text: "#8B4513",
+      textAlt: "#B8860B",
+      backgroundImage: `url(${soggaBackground})`,
+    },
+  },
   [Themes.BINGUS]: {
     [ThemeTypes.DARK]: {
       primary: "#9C3D61",
       primaryAlt: "#662840",
-      secondary: "#5e30ba",
       background: "#361129",
       backgroundAlt: "#52103B",
       text: "#FFDEEB",
@@ -63,7 +95,6 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
     [ThemeTypes.LIGHT]: {
       primary: "#f294bd",
       primaryAlt: "#c75f8d",
-      secondary: "#5e30ba",
       background: "#ffebf3",
       backgroundAlt: "#FFFAFC",
       text: "#5e3044",
@@ -71,9 +102,52 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
       backgroundImage: `url(${bingusBackground})`,
     },
   },
+  [Themes.MAXWELL]: {
+    [ThemeTypes.DARK]: {
+      primary: "#567669",
+      primaryAlt: "#000000",
+      background: "#363636",
+      backgroundAlt: "#1F1F1F",
+      text: "#FFFFFF",
+      textAlt: "#D0D0D0",
+      backgroundImage: `url(${maxwellDarkBackground})`,
+    },
+    [ThemeTypes.LIGHT]: {
+      primary: "#567669",
+      primaryAlt: "#C0C0C0",
+      background: "#EFEFEF",
+      backgroundAlt: "#F7F7F7",
+      text: "#000000",
+      textAlt: "#505050",
+      backgroundImage: `url(${maxwellBackground})`,
+    },
+  },
+  [Themes.OIIA]: {
+    [ThemeTypes.DARK]: {
+      primary: "#0050FF",
+      primaryAlt: "#FF5000",
+      background: "#000000",
+      backgroundAlt: "#1A1A1A",
+      text: "#FFDF00",
+      textAlt: "#40FF50",
+      backgroundImage: `url(${oiiaDarkBackground})`,
+    },
+    [ThemeTypes.LIGHT]: {
+      primary: "#00FFFF",
+      primaryAlt: "#FF00FF",
+      background: "#FFFFFF",
+      backgroundAlt: "#FFFF00",
+      text: "#B800B8",
+      textAlt: "#00B800",
+      backgroundImage: `url(${oiiaBackground})`,
+    },
+  },
 };
 
 export const ThemeIcons: Record<Themes, string> = {
   [Themes.FLOPPA]: floppaIcon,
+  [Themes.SOGGA]: soggaIcon,
   [Themes.BINGUS]: bingusIcon,
+  [Themes.MAXWELL]: maxwellIcon,
+  [Themes.OIIA]: oiiaIcon,
 };
