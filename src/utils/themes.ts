13a41567aacf2ce4ce9d2bf9f5a87fae4db48a7e
miruna <58366207+mirunaish@users.noreply.github.com>
2024-12-03 17:55:30 +0200

    more themes

--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -26,18 +26,44 @@ import googasIcon from "../assets/googas.png";
 import googasBackground from "../assets/googas-bg.jpg";
 import googasDarkBackground from "../assets/googas-bg-dark.jpg";
 
+import smudgeIcon from "../assets/smudge.png";
+import smudgeBackground from "../assets/smudge-bg.jpg";
+import smudgeDarkBackground from "../assets/smudge-bg-dark.jpg";
+
+import mrFreshIcon from "../assets/mr-fresh.png";
+import mrFreshBackground from "../assets/mr-fresh-bg.jpg";
+import mrFreshDarkBackground from "../assets/mr-fresh-bg-dark.png";
+
+import blackcatzoningoutIcon from "../assets/blackcatzoningout.png";
+import blackcatzoningoutBackground from "../assets/blackcatzoningout-bg.jpg";
+import blackcatzoningoutDarkBackground from "../assets/blackcatzoningout-bg-dark.png";
+
+import vibingCatIcon from "../assets/vibing-cat.png";
+import vibingCatBackground from "../assets/vibing-cat-bg.jpg";
+import vibingCatDarkBackground from "../assets/vibing-cat-bg-dark.png";
+
+import wawaIcon from "../assets/wawa.png";
+import wawaBackground from "../assets/wawa-bg.jpg";
+import wawaDarkBackground from "../assets/wawa-bg-dark.jpg";
+
 export enum ThemeTypes {
   DARK = "dark",
   LIGHT = "light",
 }
 
 export enum Themes {
-  FLOPPA = "floppa", // brown
-  SOGGA = "sogga", // yellow
-  BINGUS = "bingus", // purplish pink
-  MAXWELL = "maxwell", // greenish
   POPCAT = "popcat", // red
+  MR_FRESH = "mrFresh", // orange
+  SOGGA = "sogga", // yellow
+  SMUDGE = "smudge", // spring green
+  WAWA = "wawa", // mint
   GOOGAS = "googas", // blue
+  BLACKCATZONINGOUT = "blackcatzoningout", // purple
+  VIBING_CAT = "vibingCat", // magenta
+  BINGUS = "bingus", // pink
+
+  FLOPPA = "floppa", // brown
+  MAXWELL = "maxwell", // pine greenish
   OIIA = "oiia", // neons
 }
 
@@ -94,20 +120,20 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
   },
   [Themes.BINGUS]: {
     [ThemeTypes.DARK]: {
-      primary: "#9C3D61",
-      primaryAlt: "#662840",
-      background: "#361129",
-      backgroundAlt: "#52103B",
-      text: "#FFDEEB",
-      textAlt: "#C49BAB",
+      primary: "#b56576",
+      primaryAlt: "#843247",
+      background: "#31101f",
+      backgroundAlt: "#4d1e33",
+      text: "#f8e1e7",
+      textAlt: "#d4b0b8",
       backgroundImage: `url(${bingusDarkBackground})`,
     },
     [ThemeTypes.LIGHT]: {
-      primary: "#f294bd",
-      primaryAlt: "#c75f8d",
-      background: "#ffebf3",
-      backgroundAlt: "#FFFAFC",
-      text: "#5e3044",
+      primary: "#f4a3b5",
+      primaryAlt: "#e07a8e",
+      background: "#ffe6eb",
+      backgroundAlt: "#fff5f7",
+      text: "#5a2a35",
       textAlt: "#a85e7e",
       backgroundImage: `url(${bingusBackground})`,
     },
@@ -119,14 +145,14 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
       background: "#363636",
       backgroundAlt: "#1F1F1F",
       text: "#FFFFFF",
-      textAlt: "#D0D0D0",
+      textAlt: "#c7c7c7",
       backgroundImage: `url(${maxwellDarkBackground})`,
     },
     [ThemeTypes.LIGHT]: {
       primary: "#567669",
-      primaryAlt: "#C0C0C0",
-      background: "#EFEFEF",
-      backgroundAlt: "#F7F7F7",
+      primaryAlt: "#8f8f8f",
+      background: "#cecece",
+      backgroundAlt: "#b6b6b6",
       text: "#000000",
       textAlt: "#505050",
       backgroundImage: `url(${maxwellBackground})`,
@@ -163,7 +189,7 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
       backgroundImage: `url(${popcatDarkBackground})`,
     },
     [ThemeTypes.LIGHT]: {
-      primary: "#FF7F7F",
+      primary: "#ff6c6c",
       primaryAlt: "#FF4C4C",
       background: "#FFE4E1",
       backgroundAlt: "#FFF5F5",
@@ -178,7 +204,7 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
       primaryAlt: "#003366",
       background: "#001122",
       backgroundAlt: "#002449",
-      text: "#E0F7FF",
+      text: "#ebfaff",
       textAlt: "#97bdca",
       backgroundImage: `url(${googasDarkBackground})`,
     },
@@ -192,6 +218,106 @@ export const Colors: Record<Themes, Record<ThemeTypes, Theme>> = {
       backgroundImage: `url(${googasBackground})`,
     },
   },
+  [Themes.SMUDGE]: {
+    [ThemeTypes.DARK]: {
+      primary: "#489331",
+      primaryAlt: "#327a1b",
+      background: "#083300",
+      backgroundAlt: "#094400",
+      text: "#e7ffe0",
+      textAlt: "#abd0a0",
+      backgroundImage: `url(${smudgeDarkBackground})`,
+    },
+    [ThemeTypes.LIGHT]: {
+      primary: "#72da59",
+      primaryAlt: "#57bb3f",
+      background: "#e9ffe0",
+      backgroundAlt: "#f4fff0",
+      text: "#0e3805",
+      textAlt: "#318f22",
+      backgroundImage: `url(${smudgeBackground})`,
+    },
+  },
+  [Themes.MR_FRESH]: {
+    [ThemeTypes.DARK]: {
+      primary: "#d36d07",
+      primaryAlt: "#944a00",
+      background: "#331A00",
+      backgroundAlt: "#4D2600",
+      text: "#FFE4B5",
+      textAlt: "#FFB84D",
+      backgroundImage: `url(${mrFreshDarkBackground})`,
+    },
+    [ThemeTypes.LIGHT]: {
+      primary: "#fb9c29",
+      primaryAlt: "#da7803",
+      background: "#fdebd1",
+      backgroundAlt: "#FFF5E6",
+      text: "#4D2600",
+      textAlt: "#b36f2c",
+      backgroundImage: `url(${mrFreshBackground})`,
+    },
+  },
+  [Themes.BLACKCATZONINGOUT]: {
+    [ThemeTypes.DARK]: {
+      primary: "#7e3293",
+      primaryAlt: "#4e135e",
+      background: "#1E0F24",
+      backgroundAlt: "#2C1835",
+      text: "#f9eaff",
+      textAlt: "#BFA8D6",
+      backgroundImage: `url(${blackcatzoningoutDarkBackground})`,
+    },
+    [ThemeTypes.LIGHT]: {
+      primary: "#c657e5",
+      primaryAlt: "#971eb9",
+      background: "#F5E6F5",
+      backgroundAlt: "#FAF0FA",
+      text: "#2e1e34",
+      textAlt: "#8405b5",
+      backgroundImage: `url(${blackcatzoningoutBackground})`,
+    },
+  },
+  [Themes.VIBING_CAT]: {
+    [ThemeTypes.DARK]: {
+      primary: "#af40a4",
+      primaryAlt: "#85217b",
+      background: "#2a0d26",
+      backgroundAlt: "#3e1238",
+      text: "#f9eaff",
+      textAlt: "#d0a2d5",
+      backgroundImage: `url(${vibingCatDarkBackground})`,
+    },
+    [ThemeTypes.LIGHT]: {
+      primary: "#eb8ae1",
+      primaryAlt: "#cd57c1",
+      background: "#ffe6fc",
+      backgroundAlt: "#fff3fe",
+      text: "#4a0d46",
+      textAlt: "#b03ea3",
+      backgroundImage: `url(${vibingCatBackground})`,
+    },
+  },
+  [Themes.WAWA]: {
+    [ThemeTypes.DARK]: {
+      primary: "#008080",
+      primaryAlt: "#004C4C",
+      background: "#002626",
+      backgroundAlt: "#003D3D",
+      text: "#edffff",
+      textAlt: "#A0CFCF",
+      backgroundImage: `url(${wawaDarkBackground})`,
+    },
+    [ThemeTypes.LIGHT]: {
+      primary: "#20B2AA",
+      primaryAlt: "#008B8B",
+      background: "#E0FFFF",
+      backgroundAlt: "#F0FFFF",
+      text: "#053333",
+      textAlt: "#057575",
+      backgroundImage: `url(${wawaBackground})`,
+    },
+  },
 };
 
 export const ThemeIcons: Record<Themes, string> = {
@@ -202,4 +328,9 @@ export const ThemeIcons: Record<Themes, string> = {
   [Themes.OIIA]: oiiaIcon,
   [Themes.POPCAT]: popcatIcon,
   [Themes.GOOGAS]: googasIcon,
+  [Themes.SMUDGE]: smudgeIcon,
+  [Themes.MR_FRESH]: mrFreshIcon,
+  [Themes.BLACKCATZONINGOUT]: blackcatzoningoutIcon,
+  [Themes.VIBING_CAT]: vibingCatIcon,
+  [Themes.WAWA]: wawaIcon,
 };
